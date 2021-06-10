1fa36aceb2b48fae194e338317cadb200df91577
loriencho <42721802+loriencho@users.noreply.github.com>
2021-06-10 15:36:00 -0400

    working on room generation

--- LevelGenerator/Assets/Scripts/RoomGenerator.cs
+++ LevelGenerator/Assets/Scripts/RoomGenerator.cs
@@ -29,20 +29,25 @@ public class RoomGenerator : MonoBehaviour
 
     }
 
-   [SerializeField]
-    private Room start;   
     [Range(1, 15)]
-    private static int maxRooms;
-    [Range(50, 100)]
-    private static int minArea;
+    public int maxRooms;
 
-    public static void generateRooms() {
-        Room[] r = new Room[maxRooms*2 + 1];
-        r[1] = new Room(-50.0f, 50.0f, 50.0f, -50.0f);
-        generateRooms(r, 1);
+    [Range(50, 100)]
+    public int minArea;
+
+    public GameObject room;
+
+    public Room[] generateRooms() {
+        
+        Room[] r = new Room[maxRooms*100 + 1];
+        r[1] = new Room(-300.0f, 300.0f, 300.0f, -300.0f);
+        generateRooms(r, 1, 0);
+        return r;
     }
 
-    private static void generateRooms(Room[] rooms, int index) {
+    private void generateRooms(Room[] rooms, int index, int numRooms) {
+        if (numRooms >= maxRooms)
+            return;
 
         Room parent = rooms[index];
 
@@ -79,36 +84,71 @@ public class RoomGenerator : MonoBehaviour
 
             //left child
             rooms[index * 2] = child1;
-            generateRooms(rooms, index*2);
+            generateRooms(rooms, index*2, numRooms + 1);
             //right child
             rooms[index * 2 + 1] = child2;
-            generateRooms(rooms, index*2 + 1);
+            generateRooms(rooms, index*2 + 1, numRooms +1 );
 
 
         }
 
     }
 
-        public static List<Room> getFinalRooms(Room[] rooms){
+        private bool isBottomNode(Room[] rooms, int index){
+            if (rooms[index] == null)
+                return false;
+            if (index*2 < rooms.Length){
+                if (rooms[index*2] != null)
+                    return false;
+            }
+            return true;
+
+        }
+        public List<Room> getFinalRooms(Room[] rooms){
             List<Room> bottomNodes = new List<Room>();
+            int count =  0;
+
             for(int i = 0; i < rooms.Length; i++){
-                if ( ( i * 2 >= rooms.Length || rooms[i * 2]   == null) && (i * 2 >= rooms.Length||rooms[i * 2]   == null)  ){
+                if(rooms[i] != null)
+                    count++;
+                if (isBottomNode(rooms, i) ){
                     bottomNodes.Add(rooms[i]);
                 }
             }
-
+            print("Rooms in tree: " + count);
             return bottomNodes;
         }  
 
 
-        public static void placeRooms(List<Room> rooms){
+        public void placeRooms(List<Room> rooms){
+            for(int i = 0; i <  rooms.Count; i ++){
+                
+                float x =  (rooms[i].getPt1().x + rooms[i].getPt2().x) / 2;
+                float y =  (rooms[i].getPt1().y + rooms[i].getPt2().y) / 2;
 
+                Instantiate(room, new Vector3(x, y, 0), Quaternion.identity);
+
+            }
+
+            print(400);
 
 
         }
 
 
+        public void createRooms(){
+            Room[] rooms = generateRooms();
+            print("Length of initial rooms array: " + rooms.Length);
+            List<Room> finalRooms = getFinalRooms(rooms);
+            print("Number of bottom row nodes: " + finalRooms.Count);
 
+            placeRooms(finalRooms);
+        }
+
+
+        void Start(){
+            createRooms();
+        }
 
 }
 
