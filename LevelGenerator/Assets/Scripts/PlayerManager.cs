94bcac68251a3a4962768922ee7393b32cb333e9
loriencho <42721802+loriencho@users.noreply.github.com>
2021-06-14 23:55:12 -0400

    player movement

--- LevelGenerator/Assets/Scripts/PlayerManager.cs
+++ LevelGenerator/Assets/Scripts/PlayerManager.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public static float speed = 200.0f;
+    public float speed = 200.0f;
+    private RaycastHit2D hit; 
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W)) {
-            transform.Translate(Vector3.up * Time.deltaTime * speed);
 
+        Vector3 v = Vector3.zero; 
+        bool move = false;
+        if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W)) {
+            v = Vector3.up * Time.deltaTime * speed;
+            move=true;
     }       
         else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.S)){
-            transform.Translate(Vector3.down * Time.deltaTime * speed);
+            v = Vector3.down * Time.deltaTime * speed;
+            move=true;
 
         }
         else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.A)) {
-            transform.Translate(Vector3.left * Time.deltaTime * speed); 
+            v = Vector3.left * Time.deltaTime * speed;
+            move=true;
+
             }
 
         else if (Input.GetKey(KeyCode.RightArrow)  ||  Input.GetKey(KeyCode.D)) {
-            transform.Translate(Vector3.right * Time.deltaTime * speed); 
+            v =Vector3.right * Time.deltaTime * speed;
+            move=true;
+
 
         }
+        if (move){
+            transform.Translate(v);
+            hit = Physics2D.Raycast(transform.position, -Vector2.up);
+            if (hit.collider == null)
+                transform.Translate(-v);
+        }
+
+
         
     }
 }
